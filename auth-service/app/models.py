5a65241c82fa1f07b133a4dad4590864e5183b53
Ionut Oprisiu <ionut.oprisiu@yahoo.com>
2026-04-15 02:07:58 +0300

    security configuration

---
+++ auth-service/app/models.py
@@ -0,0 +1,11 @@
+from app.database import Base
+from sqlalchemy import Column, Integer, String
+
+class Credential(Base):
+
+    __tablename__ = "credentials"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username= Column(String, unique=True, index=True, nullable=False)
+    hashed_password= Column(String, nullable=False)
+    role= Column(String, index=True, nullable=False)
