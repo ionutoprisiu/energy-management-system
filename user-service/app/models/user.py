608227bd81a023e6ad15053381a92f27cb1cac61
Ionut Oprisiu <ionut.oprisiu@yahoo.com>
2026-04-21 00:17:01 +0300

    user-orm

--- user-service/app/models/user.py
+++ user-service/app/models/user.py
@@ -1,4 +1,12 @@
 from sqlalchemy import Column, Integer, String
 
+from app.db.database import Base
+
 class User(Base):
-    
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    address = Column(String, nullable=True)
+    role = Column(String, nullable=False)
